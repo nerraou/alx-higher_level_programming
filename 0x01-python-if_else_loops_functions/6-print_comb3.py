d5d6d4e90a075263952727bcce5bcd15aea74826
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-11 16:55:16 -0700

    islower

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 for a in range(0, 9):
     for b in range(a, 10):
-        print(f"{a}{b}", end=", ")
+        if a == b:
+            continue
+        if a == 8 and b == 9:
+            print(f"{a}{b}")
+        else:
+            print(f"{a}{b}", ", ")
 print()
