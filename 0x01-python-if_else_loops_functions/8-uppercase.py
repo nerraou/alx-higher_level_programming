559376b3871741305d88399c857083057848db01
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-11 17:30:23 -0700

    uppercase: use string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    transformed = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            transformed += chr(ord(c) - 32)
+            print("{}".format(chr(ord(c) - 32)), end="")
         else:
-            transformed += c
-    print(str)
+            print(c, end="")
+    print()
