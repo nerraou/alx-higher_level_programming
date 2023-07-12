079970cf32b17f791edd254b5c6e0be6078e92fa
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-11 17:05:58 -0700

    use string format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,7 @@ for a in range(0, 9):
     for b in range(a, 10):
         if a == b:
             continue
-        print(f"{a}{b}", end="")
         if a == 8 and b == 9:
-            print()
+            print(f"{a}{b}")
         else:
-            print(", ", end="")
+            print(f"{a}{b}", end=", ")
