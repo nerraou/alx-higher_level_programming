8aaef905ae2ca7ab40e9fea0c326b10438fb9df8
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-13 05:16:13 -0700

    fix printing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ length = len(sys.argv)
 if length == 1:
     print("0 arguments.")
 elif length == 2:
-    print("1 argument: {}".format(sys.argv[1]))
+    print("1 argument:\n1: {}".format(sys.argv[1]))
 else:
     print("{} arguments:".format(length - 1))
     for i in range(1, length):
-        print("{} : {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
