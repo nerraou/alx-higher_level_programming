3eb89697d5b2dac902403ec923a0bd34331eedd8
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-13 07:45:45 -0700

    fix if not

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     length = len(dir(hidden_4))
     for i in range(0, length):
-        if !dir(hidden_4)[i].startswith("__"):
+        if not dir(hidden_4)[i].startswith("__"):
             print(dir(hidden_4)[i])
