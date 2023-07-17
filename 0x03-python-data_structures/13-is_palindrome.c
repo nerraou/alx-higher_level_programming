b7eba9827a3803fc4288e3d29781988646755bb5
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-17 16:02:59 -0700

    is_paindrome: solution 2

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,32 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - reverse linked list
+ * @head: linked list head
+ * Return: new linked list head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next;
+	listint_t *prev;
+
+	if (!head)
+		return (NULL);
+	prev = NULL;
+	if (*head == NULL || (*head)->next == NULL)
+		return (*head);
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (prev);
+}
+
+/**
  * get_size - get list size
  * @head: list head
  * Return: list size
@@ -19,27 +45,26 @@ int get_size(listint_t *head)
 }
 
 /**
- * list_to_array - list to array
- * @head: list head
- * @p_size: size pointer
- * Return: array of list values
+ * is_lists_equals - check if two linked lists are equal
+ * @list_a: linked list head
+ * @list_b: linked list head
+ * @size: linked list size
+ * Return: 1: if equal, othwewise: 0
  */
-int *list_to_array(listint_t *head, int *p_size)
+int is_lists_equals(listint_t *list_a, listint_t *list_b, int size)
 {
-	int size;
-	int *array;
+	int i;
 
-	size = get_size(head);
-	*p_size = size;
-	array = malloc(sizeof(int) * size);
-	if (!array)
-		return (NULL);
-	for (int i = 0; i < size; i++)
+	i = 0;
+	while (i < size)
 	{
-		array[i] = head->n;
-		head = head->next;
+		if (list_a->n != list_b->n)
+			return (0);
+		i++;
+		list_a = list_a->next;
+		list_b = list_b->next;
 	}
-	return (array);
+	return (1);
 }
 
 /**
@@ -49,28 +74,24 @@ int *list_to_array(listint_t *head, int *p_size)
  */
 int is_palindrome(listint_t **head)
 {
+	int i;
 	int size;
-	int *array;
-	int start;
-	int end;
+	int half_size;
+	listint_t *half;
 
 	if (head == NULL || *head == NULL)
 		return (1);
-	array = list_to_array(*head, &size);
-	if (!array)
-		return (0);
-	start = 0;
-	end = size - 1;
-	while (start < end)
+	size = get_size(*head);
+	half = *head;
+	half_size = size / 2;
+	for (i = 0; i < half_size; i++)
 	{
-		if (array[start] != array[end])
-		{
-			free(array);
-			return (0);
-		}
-		start++;
-		end--;
+		half = half->next;
 	}
-	free(array);
-	return (1);
+	if (size % 2 == 1)
+		half = half->next;
+	reverse_listint(&half);
+	size = is_lists_equals(*head, half, half_size);
+	reverse_listint(&half);
+	return (size);
 }
