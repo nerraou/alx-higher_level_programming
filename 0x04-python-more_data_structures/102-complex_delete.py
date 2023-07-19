13dcd32328b3cd9b6938b893436db977ad578469
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-19 15:55:09 -0700

    check for None

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    if a_dictionary is None:
+        return None
     keys_to_delete = []
     for key in a_dictionary:
         if a_dictionary[key] == value:
