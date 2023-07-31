5142fe0b023c322aecca06c9ea5c3a00a683eefd
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-31 11:35:19 -0700

    simplify count list len

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    if my_list is None:
-        return
-    if (x < 0):
-        return 0
+    count = 0
+
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
+            count += 1
     except IndexError:
-        i -= 1
-    if i > 0:
-        print("")
-    return i + 1
+        pass
+    print()
+    return count
