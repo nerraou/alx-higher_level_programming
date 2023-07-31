0f18ea7cb73012eabbcf962d57aa92d45e5ca7ee
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-31 11:10:26 -0700

    refator: safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list is None:
-        return
     i = 0
+
+    if (x < 0):
+        print("")
+        return 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
     except IndexError:
-        if i > 0:
-            i = i - 1
+        i -= 1
     print("")
     return i + 1
