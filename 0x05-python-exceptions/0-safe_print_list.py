e946ac76fcdb26e6925d0d24650552d031196eef
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-31 10:58:38 -0700

    handle empty list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
     except IndexError:
-        i = i - 1
+        if i > 0:
+            i = i - 1
     print("")
     return i + 1
