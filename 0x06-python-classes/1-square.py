2358d4023e5f5ad04b5cf1ce35aa209617cefeb5
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-01 14:30:07 -0700

    square class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,17 +3,3 @@ class Square:
     __size = 0
     def __init__(self, new_size):
         self.__size = new_size
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
