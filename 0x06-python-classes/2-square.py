9983a762637fd1ed9783fe62ceea03dfbea2881c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-01 15:15:11 -0700

    fix indentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,9 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes size attribute."""
-    if type(size) != int:
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    else:
-        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
