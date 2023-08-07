9fddfc9f8935424054ef493c68de252b044303f0
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-07 14:04:54 -0700

    fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -89,9 +89,9 @@ class Rectangle:
         static method return the
         that returns the biggest rectangle based on the area
         """
-        if type(rect_1) != Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) != Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area >= rect_2.area:
             return rect_1
