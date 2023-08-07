c77e1ec8e139b7aeaf57871501478571754237dc
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-07 10:59:41 -0700

    matrix_divided

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module for matrix divided method"""
+
+
+def matrix_divided(matrix, div):
+    """divides all elements of a matrix by div"""
+
+    if (type(div) is not int and type(div) is not float):
+        raise TypeError("div must be a number")
+    if (div == 0):
+        raise ZeroDivisionError("division by zero")
+
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix "
+            "(list of lists) of integers/floats"
+        )
+    new_matrix = []
+    row_len = None
+    for row in matrix:
+        if row_len is None:
+            row_len = len(row)
+        if row_len is not len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+
+        new_row = []
+        for x in row:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError(
+                    "matrix must be a matrix "
+                    "(list of lists) of integers/floats"
+                )
+            new_row.append(round(x / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix
