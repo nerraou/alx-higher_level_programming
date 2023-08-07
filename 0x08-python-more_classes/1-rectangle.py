cfb258f420b99c3cdb82d0b3a07add31ef314532
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-07 10:42:42 -0700

    create rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""RECTANGLE"""
+
+
+class Rectangle:
+    """
+    This is rectangle empty class
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """set width value"""
+        self.__validate_width(width)
+        self.__width = width
+
+    def __validate_width(self, width):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """set height value"""
+        self.__validate_height(height)
+        self.__height = height
+
+    def __validate_height(self, height):
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
