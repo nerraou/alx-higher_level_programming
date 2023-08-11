1f858648ca46fd9be379ba147d26c4246da7af4d
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-11 14:41:43 -0700

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,8 +10,5 @@ def text_indentation(text):
     text = text.replace(":", ".")
     text = text.replace("?", ".")
     texts = text.split(".")
-    if len(texts) == 1:
-        print(texts[0])
-        return
     for t in texts:
         print(t.strip(), end="\n\n")
