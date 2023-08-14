fdbad190a30bf79cfb543486e7aa965783437419
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-14 11:04:35 -0700

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class Square that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square
+    """
+    def __init__(self, size):
+        """ init Square"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ calculate Square area """
+        return self.__size ** 2
