1a5bca4a999f749fb1e04d05aac6dda1c3b9b566
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-14 08:25:40 -0700

    is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+is kind of class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+     check is an instance
+    of class
+    """
+    return isinstance(obj, a_class)
