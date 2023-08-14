0512eb3aec8b96cc95f04771414b728dece45376
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-14 08:43:51 -0700

    inherit from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+check is inherits from
+"""
+
+def inherits_from(obj, a_class):
+    """
+     check is an instance
+    of class
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
