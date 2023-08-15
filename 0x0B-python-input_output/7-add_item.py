2d572010bce641ad00c171c45e4774a9033deb0a
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 13:00:18 -0700

    add main test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,18 @@
 import sys
 import os.path
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
-if os.path.isfile(filename):
-    values = load_from_json_file(filename)
-else:
-    values = []
+    filename = "add_item.json"
+    if os.path.isfile(filename):
+        values = load_from_json_file(filename)
+    else:
+        values = []
 
-sys.argv.pop(0)
+    sys.argv.pop(0)
 
-values.extend(sys.argv)
+    values.extend(sys.argv)
 
-save_to_json_file(values, filename)
+    save_to_json_file(values, filename)
