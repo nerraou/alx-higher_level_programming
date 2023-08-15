6917d364cfdcf3bc8eceecc61497917841c1c4e1
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 13:37:53 -0700

    add items

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """save args to file"""
-import sys
+from sys import argv
+"""save json to file"""
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+"""load json from file"""
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-if __name__ == "__main__":
-    """save json to file"""
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    """load json from file"""
-    load_from_json_file = \
-        __import__("6-load_from_json_file").load_from_json_file
+filename = "add_item.json"
 
-    filename = "add_item.json"
+try:
+    values = load_from_json_file(filename)
+except FileNotFoundError:
+    values = []
 
-    try:
-        values = load_from_json_file(filename)
-    except FileNotFoundError:
-        values = []
+sys.argv.pop(0)
 
-    sys.argv.pop(0)
+values.extend(sys.argv)
 
-    values.extend(sys.argv)
-
-    save_to_json_file(values, filename)
+save_to_json_file(values, filename)
