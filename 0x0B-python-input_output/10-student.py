55e262ce72a3040b501b51172a1f6d4752317eb8
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 13:07:39 -0700

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+class student
+"""
+
+
+class Student:
+    """
+    class student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        init student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrives dictionary representation
+        """
+        if attrs is not None:
+            lst3 = [value for value in attrs if value in self.__dict__]
+            return lst3
+        return self.__dict__
