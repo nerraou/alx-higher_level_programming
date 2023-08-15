563352fc9a2fefd0f9082081fc514aa53a1bdcbb
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 12:50:00 -0700

    student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+class student
+"""
+
+
+class Student:
+    """
+    class student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        init student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrives dictionary representation
+        """
+        return self.__dict__
