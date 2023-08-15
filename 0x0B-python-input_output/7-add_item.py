75d339d57d4b59be325a0738fa6ed3a51a70ad2b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 15:43:08 -0700

    refactor add_item and add module doc

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """save args to file"""
+
+
 from sys import argv
-"""save json to file"""
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-"""load json from file"""
+"""append arguments"""
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""load object from JSON file"""
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+"""writes an object to text file"""
 
 filename = "add_item.json"
-
 try:
     values = load_from_json_file(filename)
 except FileNotFoundError:
     values = []
 
-for i in range(1, len(argv)):
-    content.append(argv[i])
-
+values.extend(argv[1:])
 save_to_json_file(values, filename)
