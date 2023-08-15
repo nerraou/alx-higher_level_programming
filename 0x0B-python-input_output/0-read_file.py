4f74e2a0cfb3bb27ce03ce303a6c24d8cb99addf
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 10:07:14 -0700

    fix read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """ read file using with """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
