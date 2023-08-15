110072e2dd6293a88bbfea05eae0d6186d3adf20
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 13:17:39 -0700

    use try/except

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """save args to file"""
 import sys
-import os.path
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
-
     filename = "add_item.json"
-    if os.path.isfile(filename):
+
+    try:
         values = load_from_json_file(filename)
-    else:
+    except FileNotFoundError:
         values = []
 
     sys.argv.pop(0)
