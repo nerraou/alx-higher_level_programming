b2ba3f3ae94fe866c285da259c216419d0a06340
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-15 13:24:23 -0700

    move import to main

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,11 @@
 """save args to file"""
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
 if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
+
     filename = "add_item.json"
 
     try:
