5f965dc348ac30804b4900115f9da51400b74280
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-09-19 14:17:55 -0700

    fix show full table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- This script DESCIBE a table called first_table in the current database in your MySQL
-DESC first_table;
+-- This script DESCIBE a table called first_table in the current database in your M
+SHOW CREATE TABLE first_table;
