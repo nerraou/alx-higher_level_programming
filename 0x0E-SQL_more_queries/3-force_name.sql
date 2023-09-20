1053bed22480a7084e4f317998127f3d75227277
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-09-20 14:03:35 -0700

    fix: name can not be null

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- This script creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXIST force_name
+CREATE TABLE IF NOT EXISTS force_name
 (
 	id INT,
-	name VARCHAR(256)
+	name VARCHAR(256) NOT NULL
 );
