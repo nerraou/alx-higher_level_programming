f1b3d3f0a252f521c52b8bbf2235aa2c2afdbd95
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-09-20 15:53:16 -0700

    wip

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- This scipt creates the table cities on your MySQL server.
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
