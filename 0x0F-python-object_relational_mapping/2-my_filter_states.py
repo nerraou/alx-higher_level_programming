57fb67bf655b4c93972051c64a6965f645a9ecd1
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-22 23:48:43 +0100

    feat: my filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""filter states script"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+    host = "localhost"
+
+    cnx = MySQLdb.connect(
+        host=host,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = cnx.cursor()
+    sql = """
+    SELECT *
+    FROM states
+    WHERE name LIKE '{}'
+    COLLATE utf8mb4_bin
+    ORDER BY id ASC
+    """.format(name)
+    cur.execute(sql)
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    cnx.close()
