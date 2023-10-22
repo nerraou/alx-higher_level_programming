8060144b38ce45d731df0ec90d61d1510d373ca6
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-22 23:20:59 +0100

    fix: use Mysqldb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,25 +2,27 @@
 """select states script"""
 
 import sys
-import sqlalchemy
-from sqlalchemy import orm
+import MySQLdb
 
 if __name__ == "__main__":
-    metadata = sqlalchemy.MetaData()
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    host = "localhost"
 
-    idCol = sqlalchemy.Column('id', sqlalchemy.Integer(), primary_key=True)
-    nameCol = sqlalchemy.Column('name', sqlalchemy.String(256), nullable=False)
-    State = sqlalchemy.Table('states', metadata, idCol, nameCol)
+    cnx = MySQLdb.connect(
+        host=host,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    format_str = 'mysql+mysqldb://{}:{}@localhost/{}'
-    connection_string = format_str.format(username, password, database)
-    engine = sqlalchemy.create_engine(connection_string, pool_pre_ping=True)
+    cur = cnx.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    session = orm.Session(engine)
-    for state in session.query(State).order_by(State.c.id).all():
-        print("({}, '{}')".format(state.id, state.name))
-    session.close()
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    cnx.close()
