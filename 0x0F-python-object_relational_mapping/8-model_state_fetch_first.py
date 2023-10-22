0ec40542533c9b0e656a3e7564217e66ca0a7f1e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-23 00:54:58 +0100

    feat: first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A script get first state
+"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine, orm
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, password, database)
+
+    engine = create_engine(db_url)
+
+    session = orm.Session(engine)
+
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print('{}: {}'.format(state.id, state.name))
+    else:
+        print("Nothing")
