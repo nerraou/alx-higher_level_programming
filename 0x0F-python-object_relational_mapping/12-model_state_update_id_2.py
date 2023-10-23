f0b877e4769214613dec5ae39db2f02cd75ed0e3
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-10-23 01:19:32 +0100

    feat: model state update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script get first state
+"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine, orm
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, password, database)
+
+    engine = create_engine(db_url)
+
+    session = orm.Session(engine)
+
+    state = session.query(State).filter(State.id == 2).first()
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
