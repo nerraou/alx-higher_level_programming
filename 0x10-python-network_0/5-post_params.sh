517038bbf8ec36d91a499fec38859f6b19f049f9
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-11-02 16:09:24 -0700

    add -s

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post request
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
